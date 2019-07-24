0d73fd454fe14ed0484cdc482c499aadaa995c58
Jose Salgado <Josalgado03@gmail.com>
2019-07-24 01:09:06 -0700

    Created prototype GUI design

--- NestedObjectsSample/Form1.Designer.cs
+++ NestedObjectsSample/Form1.Designer.cs
@@ -28,13 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lstInstructors = new System.Windows.Forms.ListBox();
+            this.lstCourses = new System.Windows.Forms.ListBox();
+            this.lstStudents = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstInstructors
+            // 
+            this.lstInstructors.FormattingEnabled = true;
+            this.lstInstructors.Location = new System.Drawing.Point(44, 44);
+            this.lstInstructors.Name = "lstInstructors";
+            this.lstInstructors.Size = new System.Drawing.Size(194, 264);
+            this.lstInstructors.TabIndex = 0;
+            // 
+            // lstCourses
+            // 
+            this.lstCourses.FormattingEnabled = true;
+            this.lstCourses.Location = new System.Drawing.Point(274, 44);
+            this.lstCourses.Name = "lstCourses";
+            this.lstCourses.Size = new System.Drawing.Size(194, 264);
+            this.lstCourses.TabIndex = 1;
+            // 
+            // lstStudents
+            // 
+            this.lstStudents.FormattingEnabled = true;
+            this.lstStudents.Location = new System.Drawing.Point(503, 44);
+            this.lstStudents.Name = "lstStudents";
+            this.lstStudents.Size = new System.Drawing.Size(194, 264);
+            this.lstStudents.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstStudents);
+            this.Controls.Add(this.lstCourses);
+            this.Controls.Add(this.lstInstructors);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstInstructors;
+        private System.Windows.Forms.ListBox lstCourses;
+        private System.Windows.Forms.ListBox lstStudents;
     }
 }
 
